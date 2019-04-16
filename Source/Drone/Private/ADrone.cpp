ee6480075256140befc5302eb81469c2ad6fe7d7
Daniele Calanna <dev.daniele.calanna@gmail.com>
2019-04-16 15:37:03 +0200

    Add camera control

--- Source/Drone/Private/ADrone.cpp
+++ Source/Drone/Private/ADrone.cpp
@@ -32,19 +32,24 @@ void AADrone::EndPlay(const EEndPlayReason::Type EndPlayReason)
 // Called every frame
 void AADrone::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
 	if (PIDRollRateController && PIDPitchRateController && PIDYawRateController)
 	{
 		float RollCmd = PIDRollRateController->evaluate(RollTarget, GetActorRotation().Roll, DeltaTime);
 		float PitchCmd = PIDPitchRateController->evaluate(PitchTarget, GetActorRotation().Pitch, DeltaTime);
 		float YawCmd = 0.0f;
 
-		PWM1 = Thrust - YawCmd + RollCmd + PitchCmd;
-		PWM2 = Thrust + YawCmd - RollCmd + PitchCmd;
-		PWM3 = Thrust - YawCmd - RollCmd - PitchCmd;
-		PWM4 = Thrust + YawCmd + RollCmd - PitchCmd;
+		PWM1 = - YawCmd + RollCmd + PitchCmd;
+		PWM2 = + YawCmd - RollCmd + PitchCmd;
+		PWM3 = - YawCmd - RollCmd - PitchCmd;
+		PWM4 = + YawCmd + RollCmd - PitchCmd;
+
+		PWM1 = FMath::GetMappedRangeValueClamped(FVector2D(-3.0f, 3.0f), FVector2D(0.0f, 1.0f), PWM1) * Thrust;
+		PWM2 = FMath::GetMappedRangeValueClamped(FVector2D(-3.0f, 3.0f), FVector2D(0.0f, 1.0f), PWM2) * Thrust;
+		PWM3 = FMath::GetMappedRangeValueClamped(FVector2D(-3.0f, 3.0f), FVector2D(0.0f, 1.0f), PWM3) * Thrust;
+		PWM4 = FMath::GetMappedRangeValueClamped(FVector2D(-3.0f, 3.0f), FVector2D(0.0f, 1.0f), PWM4) * Thrust;
+		
 	}
+	Super::Tick(DeltaTime);
 }
 
 // Called to bind functionality to input
